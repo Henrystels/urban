703aa4c221953ec360342310e8aff094819d118a
Henrystels <145688980+Henrystels@users.noreply.github.com>
2024-06-25 00:46:45 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+hw_count = str (12)
+hours_count = str (1.5)
+course_name = 'Python'
+one_task_time = str (1.5/12)
+print("Курс: "+course_name+", всего задач:"+hw_count+", затрачено часов: "+hours_count+", среднее время выполнения "+one_task_time+" часа." )
